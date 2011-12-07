ca21f9a7be1cd18f09dfb86a2cb7118ca77d7133
ryan lee <zero.ryanlee@gmail.com>
2011-12-08 00:47:15 +0800

    demo 1

--- 1-Tally/1-Tally/MainPage.xaml.cs
+++ 1-Tally/1-Tally/MainPage.xaml.cs
@@ -10,15 +10,43 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using WindowsPhoneApp;
 
 namespace _1_Tally
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        int count = 0;
+        Setting<int> savedCount = new Setting<int>("SaveCount",0);
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+        }
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            count++;
+            CountTextBlock.Text = count.ToString("N0");
+        }
+
+        private void ResetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            count = 0;
+            CountTextBlock.Text = count.ToString("N0");
+        }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            savedCount.Value = count;
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            count = savedCount.Value;
+            CountTextBlock.Text = count.ToString("N0");
         }
     }
 }
---
+++ 1-Tally/1-Tally/Setting.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.IO.IsolatedStorage;
+
+namespace WindowsPhoneApp
+{
+    public class Setting<T>
+    {
+        public String key { get; set; }
+        private T value;
+        public T defaultValue { get; private set; }
+        private bool hasValue;
+        public Setting(String key,T defaultValue){
+            this.key = key;
+            this.defaultValue = defaultValue;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (hasValue) { return value; }
+                if (!IsolatedStorageSettings.ApplicationSettings.TryGetValue(key, out value))
+                {
+                    value = defaultValue;
+                    IsolatedStorageSettings.ApplicationSettings[key] = value;
+                }
+                hasValue = true;
+                return value;
+            }
+            set
+            {
+                IsolatedStorageSettings.ApplicationSettings[key] = value;
+                this.value = value;
+                hasValue = true;
+            }
+        }
+
+        public void ForceFresh() {
+            hasValue = false;
+        }
+
+        
+    }
+}
