e4a7f5efe65ab84c2f9754d05579cc99ebea20c9
ryan lee <zero.ryanlee@gmail.com>
2011-12-10 21:52:06 +0800

    demo1 complete

--- 1-Tally/1-Tally/Setting.cs
+++ 1-Tally/1-Tally/Setting.cs
@@ -1,53 +1,38 @@
-﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using System.IO.IsolatedStorage;
+﻿using System.IO.IsolatedStorage;
 
 namespace WindowsPhoneApp
 {
     public class Setting<T>
     {
-        public String key { get; set; }
+        public string key { get; private set; }
         private T value;
-        public T defaultValue { get; private set; }
+        private T defaultValue;
         private bool hasValue;
-        public Setting(String key,T defaultValue){
-            this.key = key;
-            this.defaultValue = defaultValue;
-        }
 
         public T Value
         {
             get
             {
-                if (hasValue) { return value; }
+                if (hasValue) return value;
                 if (!IsolatedStorageSettings.ApplicationSettings.TryGetValue(key, out value))
                 {
+                    IsolatedStorageSettings.ApplicationSettings[key] = defaultValue;
                     value = defaultValue;
-                    IsolatedStorageSettings.ApplicationSettings[key] = value;
+                    hasValue = true;
                 }
-                hasValue = true;
                 return value;
             }
             set
             {
-                IsolatedStorageSettings.ApplicationSettings[key] = value;
                 this.value = value;
+                IsolatedStorageSettings.ApplicationSettings[key] = value;
                 hasValue = true;
             }
         }
 
-        public void ForceFresh() {
-            hasValue = false;
+        public Setting(string key, T defaultValue) {
+            this.key = key;
+            this.value = defaultValue;
         }
-
-        
     }
 }
---
+++ Shared/Setting/Setting.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.IO.IsolatedStorage;
+
+namespace WindowsPhoneApp
+{
+    public class Setting<T>
+    {
+        public string key { get; private set; }
+        private T value,defaultValue;
+        private bool hasValue;
+
+        public T Value
+        {
+            get
+            {
+                if (hasValue) return value;
+                if (!IsolatedStorageSettings.ApplicationSettings.TryGetValue(key, out value))
+                {
+                    IsolatedStorageSettings.ApplicationSettings[key] = defaultValue;
+                    value = defaultValue;
+                    hasValue = true;
+                }
+                return value;
+            }
+            set
+            {
+                this.value = value;
+                IsolatedStorageSettings.ApplicationSettings[key] = value;
+                hasValue = true;
+            }
+        }
+
+        public Setting(string key, T defaultValue) {
+            this.key = key;
+            this.value = defaultValue;
+        }
+    }
+}
